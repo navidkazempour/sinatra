5075c4a54b4de57291dac8f37d254d98d120495a
Navid <n.kazempour@gmail.com>
2016-04-16 03:21:00 +0000

    The Model & Table 3

---
+++ db/migrate/20160416024747_add_message.rb
@@ -0,0 +1,10 @@
+class AddMessage < ActiveRecord::Migration
+  def change
+    create_table :messages do |t|
+      t.string :title
+      t.string :author
+      t.string :content
+    end
+
+  end
+end
